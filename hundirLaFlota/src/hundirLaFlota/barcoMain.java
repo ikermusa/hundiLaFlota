9e432ff4ce5724eb6bbc0350e23aa56eb34557e3
ikaslea <ikaslea@DESKTOP-O2E2ROF>
2022-03-14 12:51:51 +0100

    14/03/2022

--- hundirLaFlota/src/hundirLaFlota/barcoMain.java
+++ hundirLaFlota/src/hundirLaFlota/barcoMain.java
@@ -9,11 +9,20 @@ public class barcoMain {
 		Scanner teklatua = new Scanner(System.in);
 		int[] arrayBarco = new int[15];
 		int posicion= (int)(Math.random()*15);
-		int intentoUsuario,intentos=0;
+		int intentoUsuario,intentos=0,kont=0;
+		String aurkituta="ez";
+		boolean tocado=false;
 	
 		System.out.println(posicion);
-		arrayBarco[posicion]=1;
-		arrayBarco[posicion+1]=1;
+		if (posicion<14) {
+			arrayBarco[posicion]=1;
+			arrayBarco[posicion+1]=1;
+		}
+		
+		for (int i = 0; i < arrayBarco.length; i++) {
+			System.out.print(arrayBarco[i]);
+		}
+		
 		System.out.println("Barco colocado!");
 		System.out.println("---------------");
 		
@@ -22,11 +31,23 @@ public class barcoMain {
 			System.out.println("En que posicion crees que esta el barco? ");
 			intentoUsuario = teklatua.nextInt();
 			
-			for (int i = 0; i < arrayBarco.length; i++) {
-				//if (intentoUsuario == ) {
-					
-				//}
+			
+			if (arrayBarco[intentoUsuario-1]==1) {
+				System.out.println("Tocado");
+				tocado=true;
+			}else {
+				System.out.println("Agua");
+				intentos++;
 			}
+			
+			if ((arrayBarco[intentoUsuario-1]==1) && tocado) {
+				System.out.println("Hundido");
+			}else {
+				System.out.println("Agua");
+				intentos++;
+			}
+			
+			
 			
 			
 		} while (intentos!=5);
