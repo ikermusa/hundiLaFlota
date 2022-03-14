c16895d91c8daf7b01f3ec051fa2f909d5004ff7
ikaslea <ikaslea@DESKTOP-O2E2ROF>
2022-03-14 11:28:41 +0100

    proba

--- hundirLaFlota/src/hundirLaFlota/barcoMain.java
+++ hundirLaFlota/src/hundirLaFlota/barcoMain.java
@@ -1,10 +1,18 @@
 package hundirLaFlota;
+import java.util.*;
+
 
 public class barcoMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] arrayBarco = new int[15];
+		int posicion= (int)(Math.random()*15);
+		
+		arrayBarco[posicion]=1;
+		
+		
+		
 	}
 
 }
