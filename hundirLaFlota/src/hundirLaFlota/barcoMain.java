f2cd10e632487927007cb177b7bd2451cb1563d4
iker <iker@DESKTOP-P44E100.mshome.net>
2022-03-16 18:07:16 +0100

    16/03/2022

--- hundirLaFlota/src/hundirLaFlota/barcoMain.java
+++ hundirLaFlota/src/hundirLaFlota/barcoMain.java
@@ -2,6 +2,7 @@ package hundirLaFlota;
 import java.util.*;
 
 
+
 public class barcoMain {
 
 	public static void main(String[] args) {
@@ -13,15 +14,14 @@ public class barcoMain {
 		String aurkituta="ez";
 		boolean tocado=false;
 	
-		System.out.println(posicion);
 		if (posicion<14) {
 			arrayBarco[posicion]=1;
 			arrayBarco[posicion+1]=1;
 		}
 		
-		for (int i = 0; i < arrayBarco.length; i++) {
+		/*for (int i = 0; i < arrayBarco.length; i++) {
 			System.out.print(arrayBarco[i]);
-		}
+		}*/
 		
 		System.out.println("Barco colocado!");
 		System.out.println("---------------");
@@ -31,27 +31,40 @@ public class barcoMain {
 			System.out.println("En que posicion crees que esta el barco? ");
 			intentoUsuario = teklatua.nextInt();
 			
-			
+			// ACIERTA LA PRIMERA
 			if (arrayBarco[intentoUsuario-1]==1) {
 				System.out.println("Tocado");
+				System.out.println("Donde crees que esta la otra parte del barco?");
+				intentoUsuario = teklatua.nextInt();
 				tocado=true;
 			}else {
 				System.out.println("Agua");
+				if(intentos==4) {
+					System.out.println("Ultimo intento, donde crees que esta el barco?");
+					intentoUsuario = teklatua.nextInt();
+				}else {
+					System.out.println("Has fallado, donde crees que esta?");
+					intentoUsuario = teklatua.nextInt();
+				}
+				
 				intentos++;
 			}
 			
+			
+			//TIENE QUE ESTAR TOCADO PARA QUE ENTRE EN ESTA CONDICION
 			if ((arrayBarco[intentoUsuario-1]==1) && tocado) {
-				System.out.println("Hundido");
+				System.out.println("Has hundido el barco");
+				System.exit(0);
 			}else {
 				System.out.println("Agua");
 				intentos++;
 			}
-			
 			
 			
 			
 		} while (intentos!=5);
 		
+		System.exit(0);
 		
 		
 	}
